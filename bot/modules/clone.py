d1195d3196d6fede591b30a5c9b239b547aa4109
chocotar <80971016+chocotar@users.noreply.github.com>
2021-03-20 15:39:21 +0700

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -4,7 +4,7 @@ from bot.helper.telegram_helper.message_utils import *
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.bot_utils import new_thread
-from bot import dispatcher
+from bot import dispatcher, LOGGER
 
 
 @new_thread
@@ -12,13 +12,15 @@ def cloneNode(update,context):
     args = update.message.text.split(" ",maxsplit=1)
     if len(args) > 1:
         link = args[1]
+        username = f"@{update.message.from_user.username}"
         msg = f'Cloning...\n' \
               f'User: {username}\n' \
               f'Link: <a href="{link}">{link}</a>'
         sendMessage(msg, context.bot, update)
         gd = GoogleDriveHelper()
         result, button = gd.clone(link)
-        deleteMessage(context.bot,msg)
+        LOGGER.info('ID: {} - Username: {} - Message: {}'.format(update.message.chat.id,update.message.chat.username,update.message.text))
+        # deleteMessage(context.bot,msg)
         sendMarkup(result,context.bot,update,button)
     else:
         sendMessage("Provide G-Drive Shareable Link to Clone.",context.bot,update)
